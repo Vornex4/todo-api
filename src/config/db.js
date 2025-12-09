53de5989a57bb224d2c234aba4b81a827c45cf1b
Vornex4 <109313492+Vornex4@users.noreply.github.com>
2025-12-08 19:48:10 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // estas opciones en versiones nuevas a veces no son necesarias, pero no hacen daño
-    });
+    console.log('👉 MONGO_URI:', process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Conectado a MongoDB');
   } catch (error) {
     console.error('❌ Error conectando a MongoDB:', error.message);
